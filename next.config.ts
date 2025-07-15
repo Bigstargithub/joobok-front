2eb38b9606525797a3c0b7bc1aa1b9482ac2b3ec
bigstarjang1993 <qufquf2006@hanmail.net>
2025-07-15 20:42:34 +0900

    next.config.js URL 추가

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:8000/**")],
+    remotePatterns: [
+      new URL("http://localhost:8000/**"),
+      new URL(
+        "http://ec2-54-180-146-70.ap-northeast-2.compute.amazonaws.com/**"
+      ),
+    ],
   },
   /* config options here */
 };
