5d2976b5bf791978766f5b428be6d321cfccc011
bigstarjang1993 <qufquf2006@hanmail.net>
2025-07-17 22:02:23 +0900

    og 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="주복교회 | 장로회" />
+        <meta
+          property="og:description"
+          content="대한예수교 장로회 소속 천안 주복교회는 예배와 말씀, 다음세대 교육을 통해 하나님 나라를 세워가는 기독교 공동체입니다."
+        />
+        <meta property="og:image" content="/logo_pc.png" />
+        <meta property="og:image:width" content="2500" />
+        <meta property="og:image:height" content="1330" />
+        <meta property="og:url" content="https://joobok.or.kr" />
+        <meta property="og:site_name" content="Joobok" />
+        <meta property="og:type" content="website" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
